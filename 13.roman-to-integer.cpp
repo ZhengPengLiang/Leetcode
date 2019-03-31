ea8ca5e338913418058288db928c7684566ea53d
zhengpengliang <zhengpengliang@100tal.com>
2019-03-31 21:41:57 +0800

    13

--- 13.roman-to-integer.cpp
+++ 13.roman-to-integer.cpp
@@ -77,19 +77,37 @@
  * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
  * 
  */
-#include<iostream>
-#include<string.h>
-using namespace std;
+// #include<iostream>
+// #include<string.h>
+// #include<unordered_map>
+// using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
-        return 0;
+        if(s.empty()){
+            return 0;
+        }
+        unordered_map<char,int> mp={
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+            };
+        //mp.insert({{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500}, {'M',1000}});
+        int sum=mp[s.back()];
+        for(int i=s.size()-2;i>=0;i--){
+            sum+=mp[s[i]]>=mp[s[i+1]]?mp[s[i]] : -mp[s[i]];
+        }
+        return sum;
     }
 };
-int main(){
-    Solution sol;
-    string s="MCMXCIV";
-    cout<<sol.romanToInt(s)<<endl;
-    return 0;
-}
+// int main(){
+//     Solution sol;
+//     string s="MCMXCIV";
+//     cout<<sol.romanToInt(s)<<endl;
+//     return 0;
+// }
 
